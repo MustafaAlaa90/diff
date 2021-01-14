c52b49a8ea683ddb92415c8b8fc0f0dc29e70457
mustafa <mustafa.alaa90@gmail.com>
2021-01-14 02:46:18 +0200

    Remove : CFile

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,16 @@ Copyright (C) 2021 - Mustafa Ala
 int main( int argc, char* argv[] )
 {
   CONSOLE_MSG(ZONE_INFO,"Hellow World! from Development");
+  // CFile file;
+  // file.Open("/home/mustafa/Desktop/Test/file1.txt",::fmRead);
+  // std::string Time;
+  // file.GetModifiedTime(Time);
+  // CONSOLE_MSG(ZONE_INFO,"%s",Time.data());
+  std::string file1 = "/home/mustafa/Desktop/Test/file1.txt";
+  std::string file2 = "/home/mustafa/Desktop/Test/file2.txt";
+  CDiff diff(file1,file2);
+  diff.FindDiff();
+
   return 0;
 }
 
